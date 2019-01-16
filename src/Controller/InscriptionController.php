6b6b1eecb5a2f9d477b91384b76d8512c1391e06
Corentin <corentin.medina@epsi.fr>
2019-01-16 15:08:03 +0100

    For Alex

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,24 +2,20 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class InscriptionController extends AbstractController
 {
-
     /**
-     * @Route("/", name="home")
+     * @Route("/inscription", name="inscription")
      * @return Response
      */
 
     public function index(): Response
     {
-        return $this->render('pages/home.html.twig');
-
+        return $this->render('pages/inscription.html.twig');
     }
-
 }
-
-
