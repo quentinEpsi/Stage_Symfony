713344b421575ed243342784a91a97536e8604c7
Alexandre <alexandre.cros1@epsi.fr>
2019-01-30 15:10:04 +0100

    le push du desespoir

--- src/Controller/AdminprestatairedetailController.php
+++ src/Controller/AdminprestatairedetailController.php
@@ -12,13 +12,12 @@ class AdminprestatairedetailController extends AbstractController
     /**
      * @Route("/admin/adminprestatairedetail/{id}", name="adminprestatairedetail")
      * @param ArtisanRepository $repo
-     * @param $idArtisan
+     * @param Artisan $idArtisan
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function index(ArtisanRepository $repo, Artisan $idArtisan)
     {
         $artisans = $repo->find($idArtisan);
-        dump($artisans);
         return $this->render('admin/adminprestatairedetail/index.html.twig', [
             'controller_name' => 'AdminprestatairedetailController',
             'artisans' => $artisans
--- src/Controller/CommercialdemandesdetailController.php
+++ src/Controller/CommercialdemandesdetailController.php
@@ -3,24 +3,42 @@
 namespace App\Controller;
 
 use App\Entity\Artisan;
+use App\Entity\Client;
 use App\Repository\ArtisanRepository;
+use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommercialdemandesdetailController extends AbstractController
 {
     /**
-     * @Route("/commercial/commercialdemandesdetail", name="commercialdemandesdetail")
-     * @param ArtisanRepository $repo
-     * @param Artisan $idArtisan
+     * @Route("/commercial/commercialdemandesdetail/{id}", name="commercialdemandesdetail")
+     * @param ClientRepository $repo
+     * @param Client $idClient
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(ArtisanRepository $repo, Artisan $idArtisan)
+    public function index(ClientRepository $repo, Client $idClient)
     {
-        $clients = $repo->find($idArtisan);
+        $clients = $repo->find($idClient);
         return $this->render('commercial/commercialdemandesdetail/index.html.twig', [
             'controller_name' => 'CommercialdemandesdetailController',
             'clients' => $clients
+        ]);
+    }
+
+    /**
+     * @Route("/commercial/commercialdemandesdetail/{id}", name="commercialdemandesdetail")
+     * @param ArtisanRepository $artisanRepository
+     * @param Client $idClient
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
+    public function show(ArtisanRepository $artisanRepository, Client $idClient)
+    {
+        $clients = $artisanRepository->find($idClient);
+        return $this->render('commercial/commercialdemandesdetail/index.html.twig', [
+            'controller_name' => 'CommercialdemandesdetailController',
+            'clients' => $clients,
         ]);
     }
 }
