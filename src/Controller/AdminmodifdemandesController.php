b6c48d35d7120040da77e17f6d59fcd658e528b7
Alexandre <alexandre.cros1@epsi.fr>
2019-02-04 09:30:26 +0100

    push corentin

--- src/Controller/AdminmodifdemandesController.php
+++ src/Controller/AdminmodifdemandesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,4 +18,24 @@ class AdminmodifdemandesController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/admin/adminmodifdemandes/{id}", name="adminmodifdemandes")
+     * @param ClientRepository $clientRepository
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
+    public function show(ClientRepository $clientRepository, $id)
+    {
+        $infoClient = $clientRepository->find($id);
+        dump($infoClient);
+        $repo = $infoClient->getIdArtisan();
+        $infoArtisans = $repo->getValues();
+        dump($infoArtisans);
+        return $this->render('admin/adminmodifdemandes/index.html.twig', [
+            'controller_name' => 'AdminmodifdemandesController',
+            'infoClient' => $infoClient,
+            'infoArtisans' => $infoArtisans
+        ]);
+    }
 }
