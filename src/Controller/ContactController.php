cac6ef7086a559aa1a90916f36fadc18e44fc0c5
Quentin Bernard <quentin.bernard2@epsi.fr>
2019-02-07 18:47:19 +0100

    Contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,23 +16,21 @@ class ContactController extends AbstractController
      * @Route("/contact", name="contact")
      * @return Response
      */
-    public function index(Request $request, ContactNotification $notification) : Response
+    public function index(Request $request, ContactNotification $notification) : Response  // j'injecte la request de l'utilisateur
     {
-        $contact = new Contact();
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
+        $contact = new Contact();   //Création du nouveau contact
+        $form = $this->createForm(ContactType::class, $contact);  // création du formulaire, je lui donne le formulaire que je veux crée ainsi que notre contact
+        $form->handleRequest($request); // je demande au formulaire de gérer la requête
 
         if($form->isSubmitted() && $form->isValid()){
             $notification->notify($contact);
             $this->addFlash('success', 'Votre email a bien été envoyé');
-            /*
-            return $this->redirectToRoute('contact');
-            */
+           // return $this->redirectToRoute('contact');
         }
 
         return $this->render('contact/index.html.twig',[
             'current_menu' => 'contact',
-            'form'         => $form->createView()
+            'form'         => $form->createView() //j'envoie à ma vue
         ]);
     }
 
